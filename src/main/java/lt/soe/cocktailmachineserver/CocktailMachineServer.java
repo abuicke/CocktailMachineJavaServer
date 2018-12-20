4b0f6712a72189cec248e621bc6c3bde735b5c0e
abuicke <mosesjayjayjay@gmail.com>
2018-12-20 13:25:47 +0000

    server working

--- src/main/java/lt/soe/cocktailmachineserver/CocktailMachineServer.java
+++ src/main/java/lt/soe/cocktailmachineserver/CocktailMachineServer.java
@@ -1,5 +1,9 @@
 package lt.soe.cocktailmachineserver;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
+
+import static java.lang.System.exit;
 
 @SuppressWarnings("unused")
 @RestController
@@ -18,40 +27,56 @@ public class CocktailMachineServer {
 
     public static void main(String[] args) {
         SpringApplication.run(CocktailMachineServer.class, args);
+
+        try {
+            InputStream serviceAccount = CocktailMachineServer.class.getResourceAsStream("/firebase.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://drinkingmaster-96b6c.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException ioe) {
+            System.err.println("couldn't initialise firebase sdk");
+            ioe.printStackTrace();
+            exit(-1);
+        }
+
         System.out.println("server running...");
     }
 
     @RequestMapping("/get_cocktails")
     public List<Cocktail> getCocktails() {
-//        final Semaphore semaphore = new Semaphore(0);
+        final Semaphore semaphore = new Semaphore(0);
         List<Cocktail> cocktails = new ArrayList<>();
 
         cocktails.add(new Cocktail("Martini"));
 
-//        DatabaseReference ref = FirebaseDatabase.getInstance()
-//                .getReference("cocktails/");
-//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot snap : dataSnapshot.getChildren()) {
-//                    FirebaseCocktailSchema firebaseListing = snap.getValue(FirebaseCocktailSchema.class);
-//                    System.out.println("cocktail = " + firebaseListing.name);
-//                    cocktails.add(new Cocktail(firebaseListing.name));
-//                }
-//                semaphore.release();
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                throw new IllegalStateException(error.toException());
-//            }
-//        });
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("cocktails/");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    FirebaseCocktailSchema firebaseListing = snap.getValue(FirebaseCocktailSchema.class);
+                    System.out.println("cocktail = " + firebaseListing.name);
+                    cocktails.add(new Cocktail(firebaseListing.name));
+                }
+                semaphore.release();
+            }
 
-//        try {
-//            semaphore.acquire();
-//        } catch (InterruptedException e) {
-//            throw new IllegalStateException(e);
-//        }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                throw new IllegalStateException(error.toException());
+            }
+        });
+
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
 
         return cocktails;
     }
---
+++ src/main/java/lt/soe/cocktailmachineserver/FirebaseCocktailSchema.java
@@ -0,0 +1,8 @@
+package lt.soe.cocktailmachineserver;
+
+@SuppressWarnings("WeakerAccess")
+public class FirebaseCocktailSchema {
+
+    public String name;
+
+}

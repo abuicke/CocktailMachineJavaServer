68e73b2abefe55f7ab0aac8844399d1a5bd89afd
abuicke <mosesjayjayjay@gmail.com>
2018-12-20 11:55:25 +0000

    init

---
+++ src/main/java/lt/soe/cocktailmachineserver/CocktailMachineServer.java
@@ -0,0 +1,9 @@
+package lt.soe.cocktailmachineserver;
+
+public class CocktailMachineServer {
+
+    public static void main(String[] args) {
+        System.out.println("server running...");
+    }
+
+}
